26f921bd4bef335311545c766c64cb23b5fc5b13
jeff-tabios <jefftabios1@gmail.com>
2019-08-18 12:49:52 +0800

    Added list view

---
+++ JTGitSearch/Views/ListCell.swift
@@ -0,0 +1,28 @@
+//
+//  ListCell.swift
+//  JTGitSearch
+//
+//  Created by Jeff Tabios on 18/08/2019.
+//  Copyright © 2019 Jeff Tabios. All rights reserved.
+//
+
+import UIKit
+
+class ListCell: UITableViewCell {
+
+    @IBOutlet weak var itemImage: UIImageView!
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var author: UILabel!
+    @IBOutlet weak var forks: UILabel!
+    @IBOutlet weak var language: UILabel!
+    
+    var vm: RepoViewModel?{
+        didSet{
+            itemImage.load(url: vm?.image, placeholder: UIImage(named: "noposter"))
+            name.text = vm?.name
+            author.text = vm?.author
+            forks.text = vm?.forks
+            language.text = vm?.language
+        }
+    }
+}

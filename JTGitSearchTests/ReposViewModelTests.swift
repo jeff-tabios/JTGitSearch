bbbca02a527276240960acd768147c0891939249
jeff-tabios <jefftabios1@gmail.com>
2019-08-18 10:28:38 +0800

    Added repos view model tests

---
+++ JTGitSearchTests/ReposViewModelTests.swift
@@ -0,0 +1,30 @@
+//
+//  ReposViewModelTests.swift
+//  JTGitSearchTests
+//
+//  Created by Jeff Tabios on 18/08/2019.
+//  Copyright © 2019 Jeff Tabios. All rights reserved.
+//
+
+import XCTest
+@testable import JTGitSearch
+
+class ReposViewModelTests: XCTestCase {
+
+    var sut: ReposViewModel?
+    var repositories:Repositories?
+    
+    override func setUp() {
+        super.setUp()
+        
+        sut = ReposViewModel(api: API(session: URLSessionMock()))
+        
+    }
+    
+    func test_getNextPage() {
+        sut?.getNextPage()
+        
+        XCTAssertEqual(sut?.RepoViewModels.count, 1)
+    }
+
+}

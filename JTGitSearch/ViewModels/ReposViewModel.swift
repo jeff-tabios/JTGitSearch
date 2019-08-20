149f9214df59618bcb40062586b2f97846a79b71
jeff-tabios <jefftabios1@gmail.com>
2019-08-20 22:57:51 +0800

    Refactored model and added closure variables

--- JTGitSearch/ViewModels/ReposViewModel.swift
+++ JTGitSearch/ViewModels/ReposViewModel.swift
@@ -8,22 +8,12 @@
 
 import Foundation
 
-enum sortStyle: String{
-    case stars = "stars"
-    case forks = "forks"
-}
-
-enum orderStyle: String{
-    case desc = "desc"
-    case asc = "asc"
-}
-
 protocol AppendableList{
     var lastPageLoaded: Int {get set}
     var query: String {get set}
     var sort: String? {get set}
     var order: String? {get set}
-    func reload(with term:String)
+    func reload(with term:String, sort: String?, order: String?)
     func getNextPage()
 }
 
@@ -34,33 +24,39 @@ class ReposViewModel: AppendableList {
     var sort: String?
     var order: String?
     var refreshRepoList:(()-> Void)?
-    var RepoViewModels:[RepoViewModel] = [] {
+    var repoViewModels:[RepoViewModel] = [] {
         didSet{
             self.refreshRepoList?()
         }
     }
     
+    var failGetRepoList:(()-> Void)?
+    var startGetRepoList:(()-> Void)?
+    
     init(api:APIProtocol){
         self.api = api
     }
     
-    func reload(with term:String = ""){
+    func reload(with term:String = "", sort: String? = nil, order: String? = nil){
         lastPageLoaded = 0
         query = term
-        RepoViewModels = []
+        self.sort = sort
+        self.order = order
+        repoViewModels = []
         getNextPage()
     }
     
     func getNextPage(){
         if !query.isEmpty{
+            startGetRepoList?()
             api?.request(endPoint: GitHubAPI.search(q: query, page: lastPageLoaded+1, sort: sort, order: order),
                          completion: { [weak self] (result:Result<Repositories,APIServiceError>) in
                             switch result{
                             case .success(let repos):
                                 self?.lastPageLoaded += 1
-                                self?.RepoViewModels += repos.items.map{RepoViewModel(repo: $0)}
+                                self?.repoViewModels += repos.items.map{RepoViewModel(repo: $0)}
                             case .failure:
-                                break
+                                self?.failGetRepoList?()
                             }
             })
         }
